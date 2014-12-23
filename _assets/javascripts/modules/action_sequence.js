2451372e205a832577c08611ae25a3d40f27372e
Paul Hayes <fofr@users.noreply.github.com>
2014-12-23 09:17:06 +0000

    Create naive action sequence module
    Progress through actions one by one, using a type to determine the
    action to perform.

---
+++ _assets/javascripts/modules/action_sequence.js
@@ -0,0 +1,69 @@
+(function(Modules) {
+  "use strict";
+
+  Modules.ActionSequence = function() {
+    this.init = function(element) {
+
+      var actions = element.find('[data-action]'),
+          currentAction,
+          actionIndex = 0;
+
+      element.find('.hide').hide().removeClass('hide');
+      element.on('action-start', perform);
+      element.on('action-ended', progress);
+
+      progress();
+
+      function progress() {
+        if (actionIndex < actions.length) {
+          currentAction = actions.eq(actionIndex);
+          actionIndex++;
+          element.trigger('action-start');
+        }
+      }
+
+      function perform() {
+        var type = currentAction.data('action'),
+            action,
+            delay = currentAction.data('delay');
+
+        switch (type) {
+          case 'message':
+            action = message;
+            break;
+          default:
+            action = reveal;
+            break;
+        }
+
+        if (delay) {
+          setTimeout(function() {
+            action();
+          }, parseInt(delay, 10));
+        } else {
+          action();
+        }
+      }
+
+      function reveal() {
+        currentAction.fadeIn(200, function() {
+          element.trigger('action-ended');
+        });
+      }
+
+      function message() {
+        var message = currentAction.html(),
+            duration = currentAction.data('writing-duration') || 1000;
+
+        currentAction.text('…');
+        currentAction.fadeIn(200, function() {
+          setTimeout(function() {
+            currentAction.html(message);
+            element.trigger('action-ended');
+          }, duration);
+        });
+      }
+    };
+  };
+
+})(window.App.Modules);
