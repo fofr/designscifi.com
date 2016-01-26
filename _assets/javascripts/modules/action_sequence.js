f9e712ad97f148e6171cbdcb8990622e6f612b80
Paul Hayes <fofr@users.noreply.github.com>
2016-01-26 14:45:17 +0000

    Add phone focus action

--- _assets/javascripts/modules/action_sequence.js
+++ _assets/javascripts/modules/action_sequence.js
@@ -31,6 +31,9 @@
           case 'message':
             action = message;
             break;
+          case 'phone-focus':
+            action = focusPhone;
+            break;
           default:
             action = reveal;
             break;
@@ -46,31 +49,56 @@
       }
 
       function reveal() {
-        currentAction.fadeIn(200, function() {
-          element.trigger('action-ended');
-        });
+        var delay = currentAction.data('delay') || 1000;
+
+        setTimeout(function() {
+          fadeIn();
+        }, delay);
+
+        function fadeIn() {
+          currentAction.fadeIn(200, function() {
+            scrollDown();
+            element.trigger('action-ended');
+          });
+        }
+      }
+
+      function focusPhone() {
+        var delay = currentAction.data('delay') || 2000;
+        setTimeout(function() {
+          scrollDown();
+          currentAction.show().removeClass('phone-unfocused');
+          setTimeout(function() {
+            element.trigger('action-ended');
+          }, 1000);
+        }, delay)
       }
 
       function message() {
         var message = currentAction.html(),
-            delay = currentAction.data('delay') || 1000,
+            delay = currentAction.data('delay') || 1500,
             duration = currentAction.data('writing-duration') || 1000;
 
         currentAction.text('…');
 
         setTimeout(function() {
           fadeInMessage();
-        }, delay)
+        }, delay);
 
         function fadeInMessage() {
           currentAction.fadeIn(200, function() {
+            scrollDown();
             setTimeout(function() {
               currentAction.html(message);
-              //window.scrollTo(0,document.body.scrollHeight);
+              scrollDown();
               element.trigger('action-ended');
             }, duration);
           });
         }
+      }
+
+      function scrollDown() {
+        $('html,body').animate({scrollTop: document.body.scrollHeight}, 1000);
       }
     };
   };
